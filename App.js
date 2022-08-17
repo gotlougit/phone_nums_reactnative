3a43e391a74024095550c7fbd51083524ebbbdf7
Saksham Mittal <gotlouemail@gmail.com>
2022-08-17 13:46:48 +0530

    Make concurrent requests using Promise.all
    Also included in this commit:
    
    - Error messages when there is no result to be found
    
    - Prevent making bad requests by always validating the input is a 10
      digit number
    
    - Return the name in titlecase to hide inconsistencies in which promise
      fulfills first
    
    Fix faulty titlecase() implementation

--- App.js
+++ App.js
@@ -6,22 +6,15 @@ import React, { useState } from 'react';
 let suffices = ["paytm","upi","ybl","apl","okaxis","okbizaxis","okhdfcbank","okicici",
 "oksbi","waaxis","wahdfcbank","waicici","wasbi","freecharge","payzapp"];
 
-let answer = "";
+let name = "";
 
-const lookUpVPA = async (vpa, refresh) => {
-	console.log("making request for vpa: " + vpa);
+const lookUpVPA = async (vpa) => {
 	return fetch('https://upibankvalidator.com/api/upiValidation?upi=' + vpa, 
 		{method: 'POST', body: JSON.stringify({upi: vpa})})
 		.then((response) => response.json())
 		.then((json) => {
 			if (json.isUpiRegistered) {
-				if (!answer) {
-					answer = (json.name);
-					refresh();
-				}
-				return;
-			} else {
-				throw "not found";
+				return json.name;
 			}
 		})
 		.catch((error) => {
@@ -30,27 +23,59 @@ const lookUpVPA = async (vpa, refresh) => {
 };
 
 const lookUpNumber = async (number, refresh) => {
-	if (answer) {
-		answer = "";
+	if (number.toString().length != 10) {
+		name = "Error! Enter 10 digit Indian phone number!";
+		refresh();
+		return;
+	}
+	if (name) {
+		name = "";
 	}
 	const vpalist = new Array();
 	for (s in suffices) {
-		vpalist.push(lookUpVPA(number + "@" + suffices[s], refresh));
+		vpalist.push(lookUpVPA(number + "@" + suffices[s]));
 	}
-	Promise.any(vpalist).then(() => console.log(answer));
+	Promise.all(vpalist).then((returnval) => showName(returnval, refresh));
+};
+
+const showName = (resultArr, refresh) => {
+	for (r in resultArr) {
+		if (resultArr[r]) {
+			name = resultArr[r];
+			refresh();
+			return;
+		}
+	}
+	name = "Name not found!";
+	refresh();
+};
+
+const titlecase = (string) => {
+	let words = string.split(" ");
+	console.log(words);
+	let newstr = "";
+	for (w in words) {
+		words[w] = words[w].toLowerCase();
+		words[w][0].toUpperCase();
+		newstr += words[w][0].toUpperCase() + words[w].slice(1) + " ";
+		console.log("newstr: ", newstr);
+	}
+	console.log("newstr: ", newstr);
+	return newstr;
 };
 
 export default function App() {
 	const [text, setText] = useState('');
 	const [_, setValue] = useState();
 	const refresh = () => {
+		name = titlecase(name);
 		setValue({});
 	};
 	return (
 		<View style={styles.container}>
 			<Text style={{fontSize: 30}}>Phone Number Lookup</Text>
 			<TextInput autoComplete='tel-national' keyboardType='phone-pad' textContentType='telephoneNumber' maxLength={10} style={{height: 40}} placeholder="Enter phone number" onChangeText={newText => setText(newText)} defaultValue={text} />
-			<Text>{answer}</Text>
+			<Text>{name}</Text>
 			<Button title='Look It Up' onPress={() => lookUpNumber(text, refresh)}/>
 			<StatusBar style="auto" />
 		</View>
